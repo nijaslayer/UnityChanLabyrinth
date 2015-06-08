3760c9d00bbf34986355d8603069d47b99112711
dangogonta <nobisugita7@gmail.com>
2015-06-08 21:01:51 +0900

    unityちゃんガングロ

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,9 @@ namespace UnityChan
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject unityChan;
+	public Camera camera;
+	
 	Animator anime;
 	UnityChanControlScriptWithRgidBody unitychan;
 
@@ -18,14 +21,43 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+			if (Input.GetKeyDown (KeyCode.Z))
+			{
+				anime.SetTrigger("Attack");
+				anime.SetBool("AttackUnity", true);
+
+				Invoke ("AttackEnd", 0.5F);
+			}
 	
 	}
 
+
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.gameObject.tag == "Floor")
+		{
+				gameObject.transform.SetParent(c.transform);
+
+				for ( int count = 0; count < 50; count++)
+				{
+					//camera.transform.
+				}
+		}
+	}
+	
 	void OnGoal()
 	{
 		unitychan.enabled = false;
 		anime.SetBool ("Win", true);
 	}
+
+
+	public void AttackEnd()
+	{
+		anime.SetBool("AttackUnity", false);
+	}
+
 
 
 }
